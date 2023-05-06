c3ec23f94bbca90b909a557c71839e0cbaa14425
MichaelSun <zhensherlock@126.com>
2023-05-06 12:54:24 +0800

    Initialize test-template

---
+++ preload.js
@@ -0,0 +1,37 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+
+// Test helpers
+const test = {
+  assert: (ok, ...logs) => {
+    if (!ok) test.fail(...logs)
+  },
+  fail: (...logs) => test.done(false, ...logs),
+  done: (success = true, ...logs) => {
+    if (!success) logs.unshift(new Error('test failed'))
+    require('electron').ipcRenderer.send('test-done', success, ...logs)
+    process.exit(success ? 0 : 1)
+  }
+}
+
+// Example test: check that process.versions.electron
+// - is defined in the preload process
+// - has major, minor, and patch numbers
+// - the numbers are non-negative
+try {
+  const tokens = process.versions.electron.split('.', 3)
+  test.assert(tokens.length === 3)
+  for (const token of tokens) {
+    const num = Number.parseInt(token)
+    test.assert(!Number.isNaN(num))
+    test.assert(num >= 0)
+  }
+  test.done()
+} catch (err) {
+  test.fail(err)
+}
